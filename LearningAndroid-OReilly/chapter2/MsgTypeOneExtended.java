244b420461f881d6ca1aa6362815c37e1b03eb20
MatthewReinbold <matthew.reinbold@voxpopdesign.com>
2014-01-28 10:17:11 -0700

    complex example from Chapter 2; still needs to be debugged. will not compile using 'javac -d . *.java'

---
+++ LearningAndroid-OReilly/chapter2/MsgTypeOneExtended.java
@@ -0,0 +1,19 @@
+package com.marakana.examples;
+
+public class MsgTypeImplementationExtended extends MsgTypeImplementation {
+	@Override
+	public String getMsgType() {
+		return "MsgTypeImplementationExtended";
+	}
+
+	// here we Overload the getMsg() method so we now have an
+	// additional method that adds something to the getMsg() string
+	public String getMsg(String pre) {
+		return pre+" "+getMsg();
+	}
+
+	// and again we overload the getMsg() method, this time with an int
+	public String getMsg(int post) {
+		return " -- "+post;
+	}
+}
